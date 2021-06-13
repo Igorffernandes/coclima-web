f87b1f93a162dd6bd619d7dfa18cbaf1742a9732
Juliano Carvalho <juliano.carvalho@contabilizei.com.br>
2021-06-13 20:59:37 -0300

    change spaces

--- src/components/ArchiveModal/index.jsx
+++ src/components/ArchiveModal/index.jsx
@@ -19,46 +19,46 @@ const ArchiveModal = ({visible, onClose}) => {
               <Styled.ViewContainer>
                 <Styled.TextTitle>{acceptedFiles.length > 0 ? 'Arquivos enviados':'Envie seu arquivo'}</Styled.TextTitle>
                 {acceptedFiles.length === 0 ? 
-								(
-								  <Styled.ArchiveBox {...getRootProps({className: 'dropzone'})}>
-								    <input {...getInputProps()} />
+                (
+                  <Styled.ArchiveBox {...getRootProps({className: 'dropzone'})}>
+                    <input {...getInputProps()} />
                     <Styled.FileIcon />
                     <Styled.TextBody>
                       Arraste e solte seus arquivos aqui
                     </Styled.TextBody>
                   </Styled.ArchiveBox>
-								) : 
-								(
-									acceptedFiles.map(file => (
-										<Styled.ViewItem>
-											<Styled.FileItemIcon />
-											<Styled.TextItem>
-											  {file.path}
-											</Styled.TextItem>
-											<Styled.FileItemTrash />
-										</Styled.ViewItem>
-										)
-									)
-								)}
+                ) : 
+                (
+                  acceptedFiles.map(file => (
+                    <Styled.ViewItem>
+                      <Styled.FileItemIcon />
+                      <Styled.TextItem>
+                        {file.path}
+                      </Styled.TextItem>
+                      <Styled.FileItemTrash />
+                    </Styled.ViewItem>
+                    )
+                  )
+                )}
               </Styled.ViewContainer>
-							{acceptedFiles.length > 0 ? 
-							(
-								<Styled.ViewButton onClick={handleButton}>
-									<Styled.TextButton>
-										CONFIRMAR ENVIO
-									</Styled.TextButton>
-								</Styled.ViewButton>
-							)
-							:
-							(
-								<Styled.ViewButton {...getRootProps({className: 'dropzone'})}>
-									<input {...getInputProps()} />
-									<Styled.TextButton>
-										PROCURAR NO COMPUTADOR
-									</Styled.TextButton>
-								</Styled.ViewButton>
-							)
-							}
+              {acceptedFiles.length > 0 ? 
+              (
+                <Styled.ViewButton onClick={handleButton}>
+                  <Styled.TextButton>
+                    CONFIRMAR ENVIO
+                  </Styled.TextButton>
+                </Styled.ViewButton>
+              )
+              :
+              (
+                <Styled.ViewButton {...getRootProps({className: 'dropzone'})}>
+                  <input {...getInputProps()} />
+                  <Styled.TextButton>
+                    PROCURAR NO COMPUTADOR
+                  </Styled.TextButton>
+                </Styled.ViewButton>
+              )
+              }
           </BaseModal>
 }
 
--- src/components/PhotoModal/index.jsx
+++ src/components/PhotoModal/index.jsx
@@ -3,7 +3,6 @@ import React, {useCallback, useState} from 'react';
 import PropTypes from 'prop-types';
 import * as Styled from './styles';
 import {useDropzone} from 'react-dropzone';
-import { useEffect } from 'react';
 
 const PhotoModal = ({visible, onClose, handleConfirm}) => {
 	const [image, setImage] = useState();
@@ -20,54 +19,54 @@ const PhotoModal = ({visible, onClose, handleConfirm}) => {
   const {getRootProps, getInputProps, acceptedFiles} = useDropzone({onDrop})
 
   return  <BaseModal 
-            visible={visible} 
-            onClose={onClose} >
-              <Styled.ViewContainer>
-                <Styled.TextTitle>Envie sua foto</Styled.TextTitle>
-                {acceptedFiles.length === 0 ? 
-                (
-                <Styled.ArchiveBox {...getRootProps({className: 'dropzone'})}>
-									<input {...getInputProps()} />
-									<Styled.FileIcon />
-									<Styled.TextBody>
-											Arraste e solte sua foto aqui
-									</Styled.TextBody>
-									<Styled.TextBodyBold>
-											ou
-									</Styled.TextBodyBold>
-									<Styled.ViewButton>
-									  <Styled.TextButton>
-									  	PROCURE
-									  </Styled.TextButton>
-								  </Styled.ViewButton>
-                </Styled.ArchiveBox>
-                ) : 
-                (
-									<>
-										<Styled.ContainerDiv>
-												{image &&
-											    <Styled.ImageBox src={image} />
-												}
-											<Styled.FormDiv>
-												<Styled.FormText>
-													Nome
-												</Styled.FormText>
-												<Styled.FormBox>
-													<Styled.FormTextBox>
-													{acceptedFiles[0].path}
-													</Styled.FormTextBox>
-												</Styled.FormBox>
-											</Styled.FormDiv>
-										</Styled.ContainerDiv>
-										<Styled.ViewButton extraMargin="38" onClick={handleConfirm}>
-									    <Styled.TextButton>
-											  CONFIRMAR ENVIO
-									    </Styled.TextButton>
-								  	</Styled.ViewButton>
-									</>
-                )}
-              </Styled.ViewContainer>
-          </BaseModal>
+			visible={visible} 
+			onClose={onClose} >
+				<Styled.ViewContainer>
+				<Styled.TextTitle>Envie sua foto</Styled.TextTitle>
+				{acceptedFiles.length === 0 ? 
+				(
+				<Styled.ArchiveBox {...getRootProps({className: 'dropzone'})}>
+					<input {...getInputProps()} />
+					<Styled.FileIcon />
+					<Styled.TextBody>
+							Arraste e solte sua foto aqui
+					</Styled.TextBody>
+					<Styled.TextBodyBold>
+							ou
+					</Styled.TextBodyBold>
+					<Styled.ViewButton>
+						<Styled.TextButton>
+						PROCURE
+						</Styled.TextButton>
+					</Styled.ViewButton>
+				</Styled.ArchiveBox>
+				) : 
+				(
+        <>
+          <Styled.ContainerDiv>
+              {image &&
+              <Styled.ImageBox src={image} />
+              }
+            <Styled.FormDiv>
+              <Styled.FormText>
+                Nome
+              </Styled.FormText>
+              <Styled.FormBox>
+                <Styled.FormTextBox>
+                {acceptedFiles[0].path}
+                </Styled.FormTextBox>
+              </Styled.FormBox>
+            </Styled.FormDiv>
+          </Styled.ContainerDiv>
+          <Styled.ViewButton extraMargin="38" onClick={handleConfirm}>
+          <Styled.TextButton>
+              CONFIRMAR ENVIO
+          </Styled.TextButton>
+        </Styled.ViewButton>
+        </>
+				)}
+				</Styled.ViewContainer>
+			</BaseModal>
 }
 
 PhotoModal.propTypes = {
