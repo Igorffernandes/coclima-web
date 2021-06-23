ea64903eddbbc058e405d3a87950fc09e1b3ad7b
Thierry <thierrybarbosasantos@gmail.com>
2021-06-22 21:01:14 -0300

    hostinger host api

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,11 +1,4 @@
-export const url = 'http://localhost:3000';
+export const url = 'http://45.15.24.197';
 export const api = `${url}/api/v1`;
 export const auth = `${url}/login`;
 export const user = `${api}/users`;
-
-export const civilStateOptions = [
-  { value: 'solteiro', label: 'Solteiro' },
-  { value: 'casado', label: 'Casado' },
-  { value: 'viuvo', label: 'Viúvo' },
-  { value: 'divorciado', label: 'Divorciado' },
-];
