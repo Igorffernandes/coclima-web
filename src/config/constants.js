a4d2a0b92a384578dff7e0c96255e033327824b4
Thierry <thierrybarbosasantos@gmail.com>
2021-06-24 02:01:04 -0300

    new api ip

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const url = 'http://45.15.24.197';
+export const url = 'https://api.coclima.com';
 export const api = `${url}/api/v1`;
 export const auth = `${url}/login`;
 export const user = `${api}/users`;
