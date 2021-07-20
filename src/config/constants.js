feb556b0f2a3a2f294ef6b978711c4a4906550fa
Thierry <thierrybarbosasantos@gmail.com>
2021-07-20 18:13:30 -0300

    callback config web

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,10 @@
-// export const url = 'https://api.coclima.com';
-export const url = 'http://localhost:3000';
+export const url = "https://api.coclima.com";
+// export const url = 'http://localhost:3000';
 export const api = `${url}/api/v1`;
 export const auth = `${url}/login`;
 export const user = `${api}/users`;
 
-export const consumerKey = '9cfdd65a4b1003cceff8e6ca5a416c18eddba0f635d567f6f535fa6a6f75515c';
-export const consumerSecret = '49da65cd2ac206974395b84021076900532fef8555295f5c96bc90dd2f66964a';
+export const consumerKey =
+  "9cfdd65a4b1003cceff8e6ca5a416c18eddba0f635d567f6f535fa6a6f75515c";
+export const consumerSecret =
+  "49da65cd2ac206974395b84021076900532fef8555295f5c96bc90dd2f66964a";
